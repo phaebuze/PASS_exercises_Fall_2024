9d0463fdbe0e772693478841a3c7cf83d32ea49e
Phaedra Buzek <124410661+phaebuze@users.noreply.github.com>
2024-09-11 17:56:40 -0400

    Add files via upload

---
+++ Sept.11.py
@@ -0,0 +1,39 @@
+print("Hello, world!")
+
+# storing value "25" in memory. Reference that value with 'number'
+number = "25"
+
+catch = True
+
+num_int = 3
+num_float = 3.0
+
+# Changing variable values
+n1 = 10
+n1 = 20
+n1 = 30
+n2 = n1
+
+print(n1)
+print(n2)
+
+
+l1 = ["this is a string", 123, 4.5, True]
+
+n1 = 1.0
+n2 = "1.0"
+
+# Sample input
+prompt = "what is your name"
+name = input(prompt)
+
+
+# Age example
+
+age = int(input("What is your age?"))
+
+age += 5
+# Same thing as
+# age = age + 5
+
+print(age)
