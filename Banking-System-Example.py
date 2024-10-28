39568c8ff93a612ee514466b3813cd601b814e23
Phaedra Buzek <124410661+phaebuze@users.noreply.github.com>
2024-10-28 09:28:48 -0400

    Create Banking-System-Example.py
    Starter code for Oct. 30 session

---
+++ Banking-System-Example.py
@@ -0,0 +1,12 @@
+balance = 0
+
+def deposit(amount):
+    # Partner 1: Complete this function to add the amount to the balance
+    pass
+
+def withdraw(amount):
+    # Partner 2: Complete this function to subtract the amount from the balance
+    pass
+
+def display_balance():
+    print(f"Current balance: ${balance}")
